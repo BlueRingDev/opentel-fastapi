6b19c9958f8e603d2ad751a7932cd6cdf2518b71
Brandon Braner <brandon.braner@gmail.com>
2022-12-08 13:58:07 -0600

    working

--- app-2/main.py
+++ app-2/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
+from opentelemetry import trace
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from starlette.requests import Request
 
 app = FastAPI()
 
+tracer = trace.get_tracer(__name__)
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World From App 2"}
+async def root(request: Request):
+    with tracer.start_as_current_span("app-2-root") as span:
+        span.set_attribute("app-2-key", "value")
+        print(request.headers)
+        raise Exception("app-2-root exception")
+        return {"message": "Hello World From App 2"}
 
 
 @app.get("/hello/{name}")
